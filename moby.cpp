9d039e6986bb8d83fa699d9a7953b7feaf83570f
Praveen Ramanujam <praveen.ramanujam@yahoo.com>
2014-07-01 14:46:03 +0200

    adding force to body ...still no update

--- moby.cpp
+++ moby.cpp
@@ -1,45 +1,22 @@
-//
-// OpenSceneGraph Quick Start Guide
-// http://www.lulu.com/content/767629
-// http://www.openscenegraph.com/osgwiki/pmwiki.php/Documentation/QuickStartGuide
-//
-
-// Callback Example, Using an update callback to modify the scene graph
-
-#include <osgViewer/Viewer>
-#include <osgGA/TrackballManipulator>
-#include <osg/Camera>
-#include <osg/NodeCallback>
-#include <osg/Group>
-#include <osg/MatrixTransform>
-#include <osgDB/ReadFile>
-#include <osg/Notify>
 #include <Moby/RigidBody.h>
 #include <Ravelin/Pose3d.h>
 #include <Ravelin/Quatd.h>
 #include <boost/shared_ptr.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
-#include <boost/thread/thread.hpp> 
+#include <boost/thread/thread.hpp>
 #include <Moby/GravityForce.h>
-#include <Moby/Simulator.h>
-#include <Moby/BoxPrimitive.h>
-#include <Moby/Visualizable.h>
 #include <iostream>
-//#include <Moby/CSG.h>
+
 using namespace Moby;
 using namespace Ravelin;
 
-int
-main( int, char ** )
-{
+int main ( int argc, char *argv[] ){
 
-
-    // RIGID BODY
-    boost::shared_ptr< RigidBody > mBody( new RigidBody () );
-    Ravelin::SpatialRBInertiad J; 
+    boost::shared_ptr<RigidBody> mBody(new RigidBody()); // register body
+    Ravelin::SpatialRBInertiad J;
 
     // parameters required
-    Ravelin::SForced force(2,0,0,0,0,0);
+    Ravelin::SForced force(2,1,0.5,1,0,0);
     Ravelin::SVelocityd velocity(0.1,0,0,0,0,0);
     Ravelin::SMomentumd momentum(1,1,1,1,1,1);
     Ravelin::Origin3d trans(0,0,0.5);
@@ -51,36 +28,33 @@ main( int, char ** )
     Quatd quat(0,0,0,1);
     Vector3d origin(0,0,0);
     boost::shared_ptr<Pose3d> pose(new Pose3d());
-    pose->x = origin;  
+    pose->x = origin;
     pose->q = quat;
     J.m = 0.5;
     J.h;
     J.J = I;
     J.pose = pose ;
+
+    // set body properites
     mBody->set_inertia(J);
     mBody->set_enabled(true);
+    mBody->set_velocity(velocity);
 
+    while(true){
 
-    boost::shared_ptr< BoxPrimitive > collision( new BoxPrimitive () );   // register collision
-    boost::shared_ptr< 	CollisionGeometry  > geom( new CollisionGeometry() ); 
-    geom->set_single_body(mBody);
-    geom->set_geometry(collision);
+	mBody->add_force(force);
+	//mBody->sum_forces();
+	mBody->calc_fwd_dyn();
+	//boost::this_thread::sleep(boost::posix_time::milliseconds(500));
+        Pose3d P = *mBody->get_pose();
+	P.update_relative_pose(GLOBAL);
+	std::cout << P << std::endl;
+
+    }
+
     
-    mBody->geometries.push_back(geom); // do not if it is required
-    
-    // Create the viewer and set its scene data to our scene
-    //   graph created above.
-    osgViewer::Viewer viewer;
-    viewer.setSceneData(collision->create_visualization());
-    if (!viewer.getSceneData())
-        return( 1 );
 
-    // Set the clear color to something other than chalky blue.
-    viewer.getCamera()->setClearColor(
-            osg::Vec4( 1., 1., 1., 1. ) );
-
-    // Loop and render. OSG calls RotateCB::operator()
-    //   during the update traversal.
-    return( viewer.run() );
 }
+   
 
+    
